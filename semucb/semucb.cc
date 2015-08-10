c4c0d1f5fac307b5a6270a0712c4d288afb78ed1
ian-r-rose <ian.r.rose@gmail.com>
2015-08-09 17:25:49 -0700

    Reorganize so that several modules can use the same Mantle model

--- semucb/semucb.cc
+++ semucb/semucb.cc
@@ -36,6 +36,124 @@ extern "C" {
 #include <ucb_A3d.h>
 }
 
+//Class for taking two std::vector<double> objects, and constructing a linearly interpolated
+//function from the first to the second.
+class LinearInterpolatedFunction
+{
+  public:
+
+    //Do nothing constructor
+    LinearInterpolatedFunction() {}
+
+    //Constructor that calls initialize
+    LinearInterpolatedFunction( const std::vector<double> &x, const std::vector<double> &y )
+    {
+      initialize(x,y);
+    }
+
+    //Fill a boost::container::flat_map with the x,y values.
+    //The flat map has the property that the items in it are ordered,
+    //so the input vectors do not necessarily need to be ordered, just 
+    //paired such that x[i]->y[i].  The flat map also does binary searches
+    //according to a key, but unlike std::map, is implemented in a contiguous
+    //block of memory, so should have better speed and cache locality (at least in theory!)
+    void initialize( const std::vector<double> &x, const std::vector<double> &y )
+    {
+      if( x.size() != y.size() ) std::cerr<<"Vectors given to LinearInterpolatedFunction are of different lengths!"<<std::endl;
+      mapping.reserve(x.size());
+
+
+      std::vector<double>::const_iterator xit = x.begin();
+      std::vector<double>::const_iterator yit = y.begin();
+
+      for ( ; xit != x.end(); ++xit, ++yit )
+	mapping.insert( std::pair<double, double>( *xit, *yit ) );
+    }
+
+    //Evaluate the interpolation
+    virtual double operator()( const double x ) const
+    {
+      //get an iterator to the left of the requested value
+      boost::container::flat_map<double,double>::const_iterator entry = mapping.upper_bound(x);
+ 
+      //If we are after the end of the map, just return the rightmost value
+      if ( entry == mapping.end() ) return (--entry)->second;
+      //If we are before the beginning of the map, just return the leftmost value
+      if ( entry == mapping.begin() ) return (entry)->second;
+
+      //Otherwise, interpolate between the two relevant values
+      boost::container::flat_map<double,double>::const_iterator upper = entry, lower = --entry;
+      const double dx = (x - lower->first)/(upper->first - lower->first);
+      return dx*upper->second + (1.-dx)*lower->second;
+    }
+
+  private:
+    boost::container::flat_map<double, double> mapping;
+};
+
+namespace MantleModel
+{
+
+  LinearInterpolatedFunction gravity;
+  LinearInterpolatedFunction pressure;
+  LinearInterpolatedFunction reference_temperature;
+  LinearInterpolatedFunction reference_density; 
+  LinearInterpolatedFunction P_wave_speed; 
+  LinearInterpolatedFunction S_wave_speed; 
+  LinearInterpolatedFunction bulk_modulus; 
+  LinearInterpolatedFunction shear_modulus; 
+  LinearInterpolatedFunction thermal_expansivity;
+  LinearInterpolatedFunction heat_capacity;
+  LinearInterpolatedFunction dGdT;
+
+  void initialize()
+  {
+    static bool mantle_model_initialized = false; 
+    if (mantle_model_initialized ) return;
+
+    std::ifstream model_file( "mantle_model.txt" );
+
+    std::vector<double> r_vec, g_vec, p_vec, T_vec, rho_vec, vp_vec, 
+                        vs_vec, K_vec, G_vec, a_vec, c_vec, dgdt_vec;
+
+    char tmpline[1024];
+    model_file.getline(tmpline, 1024);  // read in the header
+    while ( ! model_file.eof() )
+      {
+	double r, g, p, T, rho, vp, vs, K, G, a, c, dgdt;
+	model_file >> r >> g >> p >> T >> rho >> vp >> vs >> K >> G >> a >> c >> dgdt;
+
+	//Fill the vectors of interest
+	r_vec.push_back(r);
+	g_vec.push_back(g);
+	p_vec.push_back(p);
+	T_vec.push_back(T);
+	rho_vec.push_back(rho);
+	vp_vec.push_back(vp);
+	vs_vec.push_back(vs);
+	K_vec.push_back(K);
+	G_vec.push_back(G);
+	a_vec.push_back(a);
+	c_vec.push_back(c);
+	dgdt_vec.push_back(dgdt);
+      }
+
+    //Initialize the LinearInterpolatedFunctions
+    gravity.initialize( r_vec, g_vec );
+    pressure.initialize( r_vec, p_vec );
+    reference_temperature.initialize( r_vec, T_vec );
+    reference_density.initialize( r_vec, rho_vec );
+    P_wave_speed.initialize( r_vec, vp_vec );
+    S_wave_speed.initialize( r_vec, vs_vec );
+    bulk_modulus.initialize( r_vec, K_vec );
+    shear_modulus.initialize( r_vec, G_vec );
+    thermal_expansivity.initialize( r_vec, a_vec );
+    heat_capacity.initialize( r_vec, c_vec );
+    dGdT.initialize( r_vec, dgdt_vec );
+
+    mantle_model_initialized = true;
+  }
+}
 
 static void initialize_semucb()
 {
@@ -51,47 +169,6 @@ static void initialize_semucb()
 
 namespace aspect
 {
-
-  class LinearInterpolatedFunction
-  {
-    public:
-
-      LinearInterpolatedFunction() {}
-
-      LinearInterpolatedFunction( const std::vector<double> &x, const std::vector<double> &y )
-      {
-        initialize(x,y);
-      }
-
-      void initialize( const std::vector<double> &x, const std::vector<double> &y )
-      {
-        AssertThrow( x.size() == y.size(), ExcMessage("Vectors for interpolation must be the same size") );
-        mapping.reserve(x.size());
-
-        std::vector<double>::const_iterator xit = x.begin();
-        std::vector<double>::const_iterator yit = y.begin();
-
-        for ( ; xit != x.end(); ++xit, ++yit )
-          mapping.insert( std::pair<double, double>( *xit, *yit ) );
-      }
-
-      virtual double value( const double x ) const
-      {
-        boost::container::flat_map<double,double>::const_iterator entry = mapping.upper_bound(x);
-        if ( entry == mapping.end() ) return (--entry)->second;
-        if ( entry == mapping.begin() ) return (entry)->second;
-
-        boost::container::flat_map<double,double>::const_iterator upper = entry, lower = --entry;
-        const double dx = (x - lower->first)/(upper->first - lower->first);
-        return dx*upper->second + (1.-dx)*lower->second;
-      }
-
-
-    private:
-
-      boost::container::flat_map<double, double> mapping;
-  };
-
   namespace InitialConditions
   {
     using namespace dealii;
@@ -125,16 +202,6 @@ namespace aspect
         parse_parameters (ParameterHandler &prm);
 
       private:
-
-        void load_mantle_model();
-
-        std::string filename;
-
-
-        LinearInterpolatedFunction reference_temperature;
-        LinearInterpolatedFunction heat_capacity;
-        LinearInterpolatedFunction thermal_expansivity;
-        LinearInterpolatedFunction dGdT;
     };
 
     template <int dim>
@@ -168,37 +235,7 @@ namespace aspect
       prm.leave_subsection ();
  
       initialize_semucb();
-      load_mantle_model();
-    }
-
-    template <int dim>
-    void
-    SEMUCB<dim>::load_mantle_model()
-    {
-      filename.assign("mantle_model.txt");
-      std::ifstream model_file( filename );
-
-      std::vector<double> r_vec, T_vec, a_vec, c_vec, dgdt_vec;
-
-      char tmpline[1024];
-      model_file.getline(tmpline, 1024);  // read in the header
-      while ( ! model_file.eof() )
-        {
-          double r, g, p, T, rho, vp, vs, K, G, a, c, dgdt;
-          model_file >> r >> g >> p >> T >> rho >> vp >> vs >> K >> G >> a >> c >> dgdt;
-
-          //Fill the vectors of interest
-          r_vec.push_back(r);
-          T_vec.push_back(T);
-          a_vec.push_back(a);
-          c_vec.push_back(c);
-          dgdt_vec.push_back(dgdt);
-        }
-
-        reference_temperature.initialize( r_vec, T_vec );
-        thermal_expansivity.initialize( r_vec, a_vec );
-        heat_capacity.initialize( r_vec, c_vec );
-        dGdT.initialize( r_vec, dgdt_vec );
+      MantleModel::initialize();
     }
 
     template <int dim>
@@ -221,7 +258,7 @@ namespace aspect
       get_ucb_ref_(&radius, &ref_rho, &ref_vpv, &ref_vsv, &ref_qk, &ref_qmu, &ref_vph, &ref_vsh, &ref_eta);
 
       //Get the 1D reference temperature profile at this radius
-      const double reference_T = reference_temperature.value( radius );
+      const double reference_T = MantleModel::reference_temperature( radius );
  
       //Calculate reference moduli
       const double ref_vs = 0.5 * (ref_vsv + ref_vsh); //Don't care about anisotropy at the moment, so average these
@@ -230,14 +267,14 @@ namespace aspect
       //Shortcut if ref_vs = 0
       if ( ref_vs == 0.0 ) return reference_T;
 
-      const double dvsdT = 0.5 * ref_vs * ( dGdT.value(radius) /ref_G + thermal_expansivity.value(radius) );
+      const double dvsdT = 0.5 * ref_vs * ( MantleModel::dGdT(radius) /ref_G + MantleModel::thermal_expansivity(radius) );
 
       double vs_perturbation;
       get_a3d_perturbation_(&lat, &lon, &radius_km, const_cast<char *>("S"), &vs_perturbation);
 
       const double dT = vs_perturbation*ref_vs / dvsdT;
 
-      return reference_T ;//+ dT;
+      return reference_T + dT;
     }
 
 
