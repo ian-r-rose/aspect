7c8a32b12c9ed6493b66086bede365be3c5b6a15
Ian Rose <ian.r.rose@gmail.com>
2015-08-12 20:13:40 -0700

    Work on plotting

--- semucb/plot_depth_slice.py
+++ semucb/plot_depth_slice.py
@@ -3,9 +3,10 @@ import numpy as np
 import tables
 import matplotlib.pyplot as plt
 import scipy.interpolate as interp
+import scipy.spatial as spatial
+import ssrfpy
 
 import cartopy.crs as ccrs
-
 
 def get_radius_list( node_array ):
   assert isinstance( node_array, tables.Array)
@@ -13,11 +14,14 @@ def get_radius_list( node_array ):
   hist, bin_edges = np.histogram( radii, bins=1000) # 1000 should probably be enough
 
   dr = bin_edges[1]-bin_edges[0] #width of bins
-  cutoff = 0.2 * np.max(hist) # ignore radii with fewer hits than this
+  cutoff = 0.2*np.max(hist)
 
-  radii_list = np.array([ bin_edges[i]+dr/2.0 for i in range(len(hist)) if hist[i] > cutoff ])
+  radius_list = np.array([ bin_edges[i]+dr/2.0 for i in range(len(hist)) if hist[i] > cutoff ])
 
-  return radii_list, dr
+  return radius_list, dr
+
+def get_closest_radius( r, radius_list ):
+  return radius_list[np.argmin( np.abs(r-radius_list))]
 
 
 def depth_slice_array( node_array, field_array, r, dr):
@@ -31,33 +35,80 @@ def depth_slice_array( node_array, field_array, r, dr):
   xyz_coord = np.array([ x[0] for x in reduced_data ])
 
   #Convert to longitude and latitude
-  lon =  180.0+np.arctan2( xyz_coord[:, 1], xyz_coord[:,0] ) * 180.0/np.pi
+  lon =  np.arctan2( xyz_coord[:, 1], xyz_coord[:,0] ) * 180.0/np.pi
+  lon[ np.where( lon < 0.0 ) ] += 360.0 #Get the longitudes from 0.-360.
   lat =  90.0-np.arctan2( np.sqrt(xyz_coord[:,0]*xyz_coord[:,0] + xyz_coord[:,1]*xyz_coord[:,1]), xyz_coord[:,2] ) * 180.0/np.pi
   #grab the field values
   values = np.array( [x[1] for x in reduced_data] ).ravel()
 
   return lon, lat, values
+
+def interpolate_regular_grid( lon, lat, values):
+
+  nvals = len(values)
+  resolution = np.rad2deg(np.sqrt((4.0*np.pi)/nvals))
+
+  x = np.cos( np.deg2rad(lon) )*np.cos(np.deg2rad(lat))
+  y = np.sin( np.deg2rad(lon) )*np.cos(np.deg2rad(lat))
+  z = np.sin(np.deg2rad(lat))
+
+  reg_lon = np.linspace(0., 360., 360./resolution)
+  reg_lat = np.linspace(-90., 90., 180./resolution)
+
+
+  mesh_lon, mesh_lat = np.meshgrid( reg_lon, reg_lat ) 
+  x_eval = np.cos( np.deg2rad(mesh_lon.flatten()) )*np.cos(np.deg2rad(mesh_lat.flatten()))
+  y_eval = np.sin( np.deg2rad(mesh_lon.flatten()) )*np.cos(np.deg2rad(mesh_lat.flatten()))
+  z_eval = np.sin(np.deg2rad(mesh_lat.flatten()))
+
+  tree = spatial.cKDTree( zip(x,y,z) )
+  d, inds = tree.query(zip(x_eval, y_eval, z_eval), k = 1)
+
+  #get interpolated 2d field
+  val_nearest = np.array([values[i] for i in inds])
+  val_nearest = val_nearest.reshape( mesh_lon.shape )
   
+  return mesh_lon, mesh_lat, val_nearest
 
-mesh_data = tables.open_file( sys.argv[1], mode='r')
-field_data = tables.open_file( sys.argv[2], mode='r')
-nodes = mesh_data.root.nodes
-field = field_data.root.T
 
-radii_list, dr = get_radius_list(nodes)
-lon, lat, values = depth_slice_array(nodes,field, radii_list[2], dr)
+def get_data_handle( step_number, field_name):
 
-reg_lon = np.linspace(0, 360, 181)
-reg_lat = np.linspace(-90,90,91)
-gridlon, gridlat = np.meshgrid( reg_lon, reg_lat)
-gridvals = interp.griddata( (lon,lat), values, (gridlon, gridlat) , method='nearest')
+    meshfile = 'mesh-'+str(step_number).zfill(5) +'.h5'
+    fieldfile = 'solution-'+str(step_number).zfill(5) +'.h5'
 
-ax = plt.axes(projection=ccrs.PlateCarree())
+    mesh_data = tables.open_file( meshfile, mode='r')
+    field_data = tables.open_file( fieldfile, mode='r')
+    nodes = mesh_data.root.nodes
+    field = getattr(field_data.root, field_name)
 
-ax.contourf(gridlon, gridlat, gridvals,  120,
-             transform=ccrs.PlateCarree())
+    return nodes, field
 
-ax.coastlines()
-ax.gridlines()
 
-plt.show()
+if __name__ == '__main__':
+    print("Loading data handles")
+    step_number = 0
+    if len(sys.argv) > 2:
+        step_number = sys.argv[1]
+    nodes, field = get_data_handle( step_number, 'vertical_heat_flux' )
+
+    print("Getting radius list")
+    radius_list, dr = get_radius_list(nodes)
+    evaluation_radius = get_closest_radius(3600.e3,radius_list)
+
+    print("Evaluating depth slice")
+    lon, lat, values = depth_slice_array(nodes,field, evaluation_radius, dr)
+    print("Interpolating field")
+    mesh_lon, mesh_lat, mesh_val = ssrfpy.interpolate_regular_grid(lon,lat,values,n=180)
+
+
+    print("plotting")
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_global()
+    image = ax.pcolormesh(mesh_lon, mesh_lat, mesh_val, transform=ccrs.PlateCarree())
+    #image = ax.contourf(mesh_lon, mesh_lat, mesh_val,  120,
+    #                    transform=ccrs.PlateCarree())
+    ax.coastlines()
+    ax.gridlines()
+
+    plt.colorbar(image)
+    plt.show()
